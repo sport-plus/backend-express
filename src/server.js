6a9e34b106a63e6e41775e542d4523218aeb5418
huydeve <haihuy129@gmail.com>
2023-06-25 11:44:37 +0700

    update cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-// const cors = require('cors');
+const cors = require('cors');
 var createError = require('http-errors');
 var path = require('path');
 const swaggerUI = require('swagger-ui-express');
